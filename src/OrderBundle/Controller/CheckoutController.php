703dce10dc121bcdef8c8cd05207c46f7be183a9
kisphp <mariusbogdan83@gmail.com>
2017-02-04 00:13:06 +0100

    checkout view

---
+++ src/OrderBundle/Controller/CheckoutController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Kisphp\OrderBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @Template()
+ */
+class CheckoutController extends Controller
+{
+    public function indexAction()
+    {
+        return [];
+    }
+}
