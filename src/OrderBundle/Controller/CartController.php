d7e0141fa3df0c53c5e3e811ab916b2adb57825e
kisphp <mariusbogdan83@gmail.com>
2017-02-04 16:50:44 +0100

    add, update and remove from cart

--- src/OrderBundle/Controller/CartController.php
+++ src/OrderBundle/Controller/CartController.php
@@ -9,6 +9,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CartController extends Controller
 {
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
     public function addAction(Request $request)
     {
         $idProduct = $request->request->getInt('idProduct');
@@ -19,6 +24,59 @@ class CartController extends Controller
         $cart = $this->get('cart.factory')->getCart();
 
         $cart->addProduct($product, $quantity);
+
+        $data = [
+            'code' => Response::HTTP_OK,
+            'cart' => [
+                'totalPrice' => $cart->getTotalPrice(),
+                'totalQuantity' => $cart->getCartItemsCount(),
+            ],
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function updateAction(Request $request)
+    {
+        $idProduct = $request->request->getInt('idProduct');
+        $quantity = $request->request->getInt('quantity');
+
+        $product = $this->get('model.articles')->getArticleById($idProduct);
+
+        $cart = $this->get('cart.factory')->getCart();
+
+        $cart->substractQuantity($product, $quantity);
+
+        $data = [
+            'code' => Response::HTTP_OK,
+            'cart' => [
+                'totalPrice' => $cart->getTotalPrice(),
+                'totalQuantity' => $cart->getCartItemsCount(),
+            ],
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function removeAction(Request $request)
+    {
+        $idProduct = $request->request->getInt('idProduct');
+
+        $product = $this->get('model.articles')->getArticleById($idProduct);
+
+        $cart = $this->get('cart.factory')->getCart();
+
+        $cart->removeProduct($product);
 
         $data = [
             'code' => Response::HTTP_OK,
